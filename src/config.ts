4a0b493c1d049381985c271b0fc0c13b4e75fa03
Rafael Siqueira <rafael.siqueira@onesight.com.br>
2021-12-02 08:37:19 -0300

    Lint .ts files with eslint and prettier

--- src/config.ts
+++ src/config.ts
@@ -1,9 +1,9 @@
-import Phaser from "phaser";
+import Phaser from 'phaser';
 
 const config: Phaser.Types.Core.GameConfig = {
   type: Phaser.AUTO,
-  parent: "game",
-  backgroundColor: "#33A5E7",
+  parent: 'game',
+  backgroundColor: '#33A5E7',
   scale: {
     width: 800,
     height: 600,
